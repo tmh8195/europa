f7d9a170be0041307de4e33d0e611b9d654502ee
Tim Hogan <timotehogan@gmail.com>
2019-01-02 22:49:45 -0500

    add trailing slash

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, '/europa/dist')));
+app.use(express.static(path.join(__dirname, '/europa/dist/europa/')));
 
 app.get('/*', function(req,res) {
 
