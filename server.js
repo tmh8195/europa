b1485f5f8c6714e982bfe517295b133e31c002fc
Tim Hogan <timotehogan@gmail.com>
2019-01-02 21:45:40 -0500

    change dir

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/europa'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname,'/dist/europa/index.html'));
+res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
